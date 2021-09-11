eb692150af1d6ef0534bf79a2483044bb09f5092
Melih Berkay <maydin13@ku.edu.tr>
2021-09-11 19:45:18 +0300

    Auth context change

--- src/components/store/auth-context.jsx
+++ src/components/store/auth-context.jsx
@@ -1,14 +1,47 @@
 //This is not a component, just a storage
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
   onLogout: () => {},
+  onLogin: (email, password) => {},
 });
 
-const AuthContextProvider = () => {
-  return <AuthContext.Provider></AuthContext.Provider>;
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLogginInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLogginInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContext;
